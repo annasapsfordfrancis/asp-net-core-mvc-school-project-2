19552e54ed5254d40c1a2d2f0a241726c0970051
Ben Sztucki <ben.sztucki@txpimpact.com>
2022-10-27 17:02:20 +0100

    example of populating a dropdown

---
+++ SchoolProject/SchoolProject.MVC/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SchoolProject.Models;
+using SchoolProject.MVC.Models;
+
+namespace SchoolProject.MVC.Controllers
+{
+
+
+ 
+
+    public class UserController : Controller
+    {
+        private readonly SchoolProjectDbContext _context;
+        public UserController(SchoolProjectDbContext context)
+        {
+            _context = context;
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var viewModel = new AddUserViewModel {
+                User = new User { },
+                SchoolList = _context.School.ToList()
+            };
+
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ SchoolProject/SchoolProject.MVC/Models/AddUserViewModel.cs
@@ -0,0 +1,12 @@
+﻿using SchoolProject.Models;
+
+namespace SchoolProject.MVC.Models
+{
+    public class AddUserViewModel
+    {
+
+        public User User { get; set; }
+        public List<School> SchoolList { get; set; }
+        public List<UserType> UserTypes { get; set; }
+    }
+}
