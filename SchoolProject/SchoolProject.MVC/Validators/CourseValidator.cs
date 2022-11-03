f125be417efe8d78d46560eb11013d9e63e4f732
Anna Sapsford-Francis <annasapsfordfrancis@gmail.com>
2022-11-03 11:14:58 +0000

    Add edit views for school, course and user

--- SchoolProject/SchoolProject.MVC/Validators/CourseValidator.cs
+++ SchoolProject/SchoolProject.MVC/Validators/CourseValidator.cs
@@ -7,8 +7,16 @@ namespace SchoolProject.Validators
     {
         public CourseValidator()
         {
-            RuleFor(course => course.CourseName).NotNull().WithMessage("Please enter a course name");
-            RuleFor(course => course.CourseDescription).NotNull().WithMessage("Please enter a course description");
+            RuleFor(course => course.CourseName)
+                .NotNull().WithMessage("Please enter a course name")
+                .NotEmpty().WithMessage("Please enter a course name")
+                .MinimumLength(3).WithMessage("The course name must have at least 3 characters")
+                .MaximumLength(30).WithMessage("The course name mustn't have more than 30 characters");
+
+            RuleFor(course => course.CourseDescription)
+                .NotNull().WithMessage("Please enter a course description")
+                .NotEmpty().WithMessage("Please enter a course description")
+                .MinimumLength(3).WithMessage("The course description must have at least 3 characters");
         }
     }
 }
--- SchoolProject/SchoolProject.MVC/Validators/SchoolValidator.cs
+++ SchoolProject/SchoolProject.MVC/Validators/SchoolValidator.cs
@@ -7,7 +7,11 @@ namespace SchoolProject.Validators
     {
         public SchoolValidator()
         {
-            RuleFor(school => school.SchoolName).NotNull().WithMessage("Please enter a school name");
+            RuleFor(school => school.SchoolName)
+                .NotNull().WithMessage("Please enter a school name")
+                .NotEmpty().WithMessage("Please enter a school name")
+                .MinimumLength(3).WithMessage("The school name must have at least 3 characters")
+                .MaximumLength(30).WithMessage("The school name mustn't have more than 30 characters");
         }
     }
 
